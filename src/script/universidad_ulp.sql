f306ed6e5065d85cec3d676afb39aa726a74c8c0
Victor Malovini <malovinivjd@hotmail.com>
2024-05-14 17:52:56 -0300

    correccion script

--- src/script/universidad_ulp.sql
+++ src/script/universidad_ulp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2024 a las 16:54:33
+-- Tiempo de generación: 14-05-2024 a las 22:48:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,34 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `universidad_ulp`
 --
+CREATE DATABASE IF NOT EXISTS `universidad_ulp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `universidad_ulp`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `idAlumno` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  PRIMARY KEY (`idAlumno`),
+  UNIQUE KEY `dni` (`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES
+(1, 132456, 'gomez', 'maximo', '2004-05-13', 1),
+(2, 2324256, 'villegas', 'guillermo', '2000-06-11', 1),
+(3, 1324346, 'diaz', 'gonzalo', '2001-07-08', 0);
 
 -- --------------------------------------------------------
 
@@ -27,7 +55,6 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `inscripcion`
 --
 
-DROP TABLE IF EXISTS `inscripcion`;
 CREATE TABLE IF NOT EXISTS `inscripcion` (
   `idInscripto` int(11) NOT NULL AUTO_INCREMENT,
   `nota` int(11) NOT NULL,
@@ -48,6 +75,31 @@ INSERT INTO `inscripcion` (`idInscripto`, `nota`, `idAlumno`, `idMateria`) VALUE
 (16, 7, 2, 4),
 (17, 8, 3, 4),
 (18, 6, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE IF NOT EXISTS `materia` (
+  `idMateria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `anio` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  PRIMARY KEY (`idMateria`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `materia`
+--
+
+INSERT INTO `materia` (`idMateria`, `nombre`, `anio`, `estado`) VALUES
+(1, 'Programacion Web', 1, 1),
+(2, 'Ingenieria de Software', 3, 0),
+(3, 'Matematica II', 2, 1),
+(4, 'Laboratorio de Programacion', 1, 0);
 
 --
 -- Restricciones para tablas volcadas
